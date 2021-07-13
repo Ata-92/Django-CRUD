bd722a89dbe35c663dafb4d5578ecd0b3be449e2
Ata-92 <atakal92@gmail.com>
2021-07-13 22:11:39 +0300

    Add postgresql database as default

--- src/clarusway/settings.py
+++ src/clarusway/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'clarusway.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clarusway',
+        'USER': 'postgres',
+        'PASSWORD': 'a;:b123cA',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
