8c43916d8b1b32e9c5f1cf591bf77b535676c557
Ata-92 <atakal92@gmail.com>
2021-07-17 13:51:26 +0300

    Import a TemplateView and add it

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from fscohort.views import home_page, student_add, student_delete, student_detail, student_list, student_update
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", home_page, name="home"),
+    # path("", home_page, name="home"),
+    path("", TemplateView.as_view(template_name="fscohort/home.html"), name="home"),
     path("list/", student_list, name="list"),
     path("add/", student_add, name="add"),
     path('<int:id>/', student_detail, name="detail"),
